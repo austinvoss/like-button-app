054f721135da38894c0839ff0bbf09343a712018
Voss <vossaustin@gmail.com>
2023-06-15 22:14:55 -0700

    simplified app to match actual twitter behavior

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,15 @@ import "./App.css";
 function App() {
   const [likes, setLikes] = useState(0);
   const [emoji, setEmoji] = useState("🤍");
-  const [isClicked, setIsClicked] = useState(false);
 
   const handleClick = () => {
     setLikes(likes + 1);
     setEmoji("🩷");
-    setIsClicked(true);
   };
 
   return (
-    <p onClick={handleClick} className={isClicked ? "clicked" : ""}>
-      {emoji} {likes}
+    <p onClick={handleClick}>
+      {emoji} {likes > 0 ? likes : ""}
     </p>
   );
 }
